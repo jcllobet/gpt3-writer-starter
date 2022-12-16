b0bbc5b4df9ea3194e3e8ca38fabd24328110744
Jan Carbonell <jcllobet@gmail.com>
2022-12-16 16:25:33 -0500

    initial boilerplate

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,59 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from "react";
+
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
 
 const Home = () => {
-  return (
-    <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>sup, insert your headline here</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
-          </div>
+    const [userInput, setUserInput] = useState("");
+    const onUserChangedText = (event) => {
+        //console.log(event.target.value);
+        setUserInput(event.target.value);
+    };
+    return (
+        <div className="root">
+            <Head>
+                <title>GPT-3 Managerial Writer</title>
+            </Head>
+            <div className="container">
+                <div className="header">
+                    <div className="header-title">
+                        <h1>GPT3 Management Writing</h1>
+                    </div>
+                    <div className="header-subtitle">
+                        <h2>Helping you think and communicate structurally</h2>
+                    </div>
+                </div>
+                <div className="prompt-container">
+                    <textarea
+                        className="prompt-box"
+                        placeholder="start typing here"
+                        value={userInput}
+                        onChange={onUserChangedText}
+                    />
+                    <div className="prompt-buttons">
+                        <a className="generate-button" onClick={null}>
+                            <div className="generate">
+                                <p>Generate</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="badge-container grow">
+                <a
+                    href="https://buildspace.so/builds/ai-writer"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <div className="badge">
+                        <Image src={buildspaceLogo} alt="buildspace logo" />
+                        <p>build with buildspace</p>
+                    </div>
+                </a>
+            </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Home;
