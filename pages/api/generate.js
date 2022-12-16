fdc2f611b8691a91b17d7746de3a9bba5d6e6ca5
Jan Carbonell <jcllobet@gmail.com>
2022-12-16 17:42:51 -0500

    hooked to openAI

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,3 +5,21 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
+const basePromptPrefix = "";
+const generateAction = async (request, response) => {
+    // Run first prompt
+    console.log(`API: ${basePromptPrefix}${request.body.userInput}`);
+
+    const baseCompletion = await openai.createCompletion({
+        model: "text-ada-001",
+        prompt: `${basePromptPrefix}${request.body.userInput}`,
+        temperature: 0.5,
+        max_tokens: 250
+    });
+
+    const basePromptOutput = baseCompletion.data.choices.pop();
+
+    response.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
